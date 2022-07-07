4cd8d9bd50303344668a0d2146c5d8cc3413bd68
G4st3r21 <70285259+G4st3r21@users.noreply.github.com>
2022-07-07 14:51:41 +0300

    Add files via upload

--- Statistics/admin.py
+++ Statistics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CollectedFriendshipsIRT, CollectedFriendshipsBAT
 
 # Register your models here.
+
+admin.site.register(CollectedFriendshipsIRT)
+admin.site.register(CollectedFriendshipsBAT)
