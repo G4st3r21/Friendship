f3424e163c07e4e7767453dcee010f7fa9f5a672
G4st3r21 <70285259+G4st3r21@users.noreply.github.com>
2022-07-26 15:19:47 +0300

    Add files via upload

--- Statistics/views.py
+++ Statistics/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import CollectedFriendshipsIRT
+from .models import CollectedFriendshipsIRT, CollectedFriendshipsBAT
 from datetime import date
 
 
@@ -11,10 +11,15 @@ def response_stats(request):
          [
              guild.color,
              guild.friendships_count,
-             guild.cardholder_name
+             guild.cardholder_name,
+             pos
          ])
-        for guild in CollectedFriendshipsIRT.objects.all()
+        for pos, guild in enumerate(CollectedFriendshipsIRT.objects.all().order_by("friendships_count").reverse())
     )
+
+    # print(type(CollectedFriendshipsBAT.objects.all()))
+    # for guild in CollectedFriendshipsBAT.objects.all():
+    #     print(type(guild))
 
     today = date.today().strftime("%d.%m.%Y")
 
