17516b467180f6a12473d02676628d2c96557140
G4st3r21 <aporyadin003@gmail.com>
2022-07-26 15:53:14 +0300

    final end tochno

--- Friendship/settings.py
+++ Friendship/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.core.checks import database
@@ -113,9 +114,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-
-import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
