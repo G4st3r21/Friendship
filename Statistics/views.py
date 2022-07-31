01a34a5e01c493d3010863dc07e597aa138e2a4d
G4st3r21 <aporyadin003@gmail.com>
2022-07-31 04:48:13 +0300

    Переход с бд на парсинг основной экономической таблицы

--- Statistics/views.py
+++ Statistics/views.py
@@ -1,31 +1,53 @@
 from django.shortcuts import render
-from .models import CollectedFriendshipsIRT, CollectedFriendshipsBAT
-from datetime import date
+from openpyxl import load_workbook
+from datetime import date as dt
+from Friendship.settings import BASE_DIR
 
 
 # Create your views here.
 
 def response_stats(request):
-    friendships_count = dict(
-        (guild.guild_name,
-         [
-             guild.color,
-             guild.friendships_count,
-             guild.cardholder_name,
-             pos
-         ])
-        for pos, guild in enumerate(CollectedFriendshipsIRT.objects.all().order_by("friendships_count").reverse())
+    wb = load_workbook(str(BASE_DIR) + '/Экономика.xlsx', data_only=True)
+    ws = wb.worksheets[2]
+    config_ws = wb.worksheets[-1]
+
+    names = [(ws.cell(row, 2).value, row) for row in range(3, 7)]
+    money = [
+        [
+            [name[0][3:], ws.cell(name[1], i).value, ws.cell(name[1], i).column]
+            for i in range(3, 16) if ws.cell(name[1], i).value is not None][-1]
+        for name in names
+    ]
+    config_info = [
+        [config_ws.cell(row, col).value for col in range(1, 4)]
+        for row in range(1, 5)
+    ]
+
+    friendship = dict(
+        (
+            name[0][3:],
+            [
+                config_info[pos][1], money[pos][1], config_info[pos][-1]
+            ]
+        ) for pos, name in enumerate(names)
     )
 
-    # print(type(CollectedFriendshipsBAT.objects.all()))
-    # for guild in CollectedFriendshipsBAT.objects.all():
-    #     print(type(guild))
+    actual_date = ws.cell(2, money[0][-1]).value
 
-    today = date.today().strftime("%d.%m.%Y")
+    games = [
+        [
+            ws.cell(row, 1).value, ws.cell(row, 2).value,
+            [
+                (col - 2, ws.cell(row, col).value) for col in range(3, 7)
+            ]
+        ]
+        for row in range(13, 30) if ws.cell(row, 1).value is not None and ws.cell(row, 3).value != '-'
+    ]
 
     context = {
-        "friendships": friendships_count,
-        "today": today
+        "friendships": friendship,
+        "games": games,
+        "today": actual_date,
     }
 
     return render(request, 'statistics/Stats.html', context=context)
